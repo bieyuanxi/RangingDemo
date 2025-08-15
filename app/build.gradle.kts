9b1dc3df5fad113e51359cf0b030712682032e31
Terra <bieyuanxi@163.com>
2025-08-15 23:07:38 +0800

    update minSdk, targetSdk and compileSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.rangingdemo"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.rangingdemo"
-        minSdk = 26
-        targetSdk = 34
+        minSdk = 29
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
