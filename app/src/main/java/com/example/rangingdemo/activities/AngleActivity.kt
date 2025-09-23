e3c700c9131250d3996dfa58edea42256b52f396
Terra <bieyuanxi@163.com>
2025-09-23 18:01:05 +0800

    fix ui theme path

--- app/src/main/java/com/example/rangingdemo/activities/AngleActivity.kt
+++ app/src/main/java/com/example/rangingdemo/activities/AngleActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -18,10 +17,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.rangingdemo.activities.ui.theme.RangingDemoTheme
+import com.example.rangingdemo.ui.theme.RangingDemoTheme
 import com.example.rangingdemo.viewmodel.RotationAngleViewModel
 
 class AngleActivity : ComponentActivity() {
