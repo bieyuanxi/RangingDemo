5354823e59367610c03fbcde5007df316bcb41cc
Terra <bieyuanxi@163.com>
2025-12-24 11:07:00 +0800

    允许滚动以展示图表

--- app/src/main/java/com/example/rangingdemo/activities/RangingActivity.kt
+++ app/src/main/java/com/example/rangingdemo/activities/RangingActivity.kt
@@ -14,6 +14,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
@@ -223,7 +225,11 @@ class RangingActivity : ComponentActivity() {
                         initialValue = emptyList()
                     )
 
-                    Column(modifier = Modifier.padding(innerPadding)) {
+                    Column(modifier = Modifier
+                        .padding(innerPadding)
+                        .fillMaxSize()
+                        .verticalScroll(rememberScrollState()) // 开启垂直滚动
+                    ) {
                         Column(
                             modifier = Modifier.fillMaxWidth(),
                             horizontalAlignment = Alignment.CenterHorizontally
--- app/src/main/java/com/example/rangingdemo/ui/components/chart.kt
+++ app/src/main/java/com/example/rangingdemo/ui/components/chart.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -133,7 +135,7 @@ fun MpChartWithStateFlow(
     }
 
     AndroidView(
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier.fillMaxSize().height(400.dp),
         factory = { context ->
             Log.d("LineChartInit", "AndroidView LineChart init")
             LineChart(context).apply {
